1b2bbfeff71c20c73edb732505afa43b14966735
Gal Noy <109943831+Gal-Noy@users.noreply.github.com>
2023-03-21 13:06:25 +0200

    finished q1 in p2

--- Assignment1/src/part2/part2.ts
+++ Assignment1/src/part2/part2.ts
@@ -1,9 +1,25 @@
 import * as R from "ramda";
+import { Dictionary } from "ramda";
 
 const stringToArray = R.split("");
 
+type LetterCount = Dictionary<number>;
+
 /* Question 1 */
-export const countLetters: undefined = undefined
+export const countLetters: Function = (str : string) : LetterCount => {
+    const addOrInc = (freqs: LetterCount, c: string): LetterCount => {
+        const newFreqs = { ...freqs };
+        const currentCount = newFreqs[c] || 0;
+        newFreqs[c] = currentCount + 1;
+        return newFreqs;
+    }
+    const isLetter = (str : string) : boolean => (str.length === 1) && (/[a-z]/i.test(str));
+    const filterLetters = (strs : string[]) : string[] => R.filter(isLetter, strs);
+    const mapToLowercase = (strs: string[]) : string[] => R.map((x: string) : string => x.toLowerCase(), strs);
+    const mapAndFilter = R.pipe(filterLetters, mapToLowercase);
+
+    return R.reduce(addOrInc, {}, mapAndFilter(stringToArray(str)));
+}
 
 /* Question 2 */
 export const isPaired: undefined = undefined
