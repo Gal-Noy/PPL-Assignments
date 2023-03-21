f22ac9d39e4ccf079d95e26bf2ddeaa9c92f4ece
Yairderry <77237113+Yairderry@users.noreply.github.com>
2023-03-21 19:31:40 +0200

    Finished question 3 part 2

--- Assignment1/src/part2/part2.ts
+++ Assignment1/src/part2/part2.ts
@@ -6,44 +6,69 @@ const stringToArray = R.split("");
 type LetterCount = Dictionary<number>;
 
 /* Question 1 */
-export const countLetters: Function = (str : string) : LetterCount => {
-    const addOrInc = (freqs: LetterCount, c: string): LetterCount => {
-        const newFreqs = { ...freqs };
-        const currentCount = newFreqs[c] || 0;
-        newFreqs[c] = currentCount + 1;
-        return newFreqs;
-    }
-    const isLetter = (str : string) : boolean => (str.length === 1) && (/[a-z]/i.test(str));
-    const filterLetters = (strs : string[]) : string[] => R.filter(isLetter, strs);
-    const mapToLowercase = (strs: string[]) : string[] => R.map((x: string) : string => x.toLowerCase(), strs);
-    const mapAndFilter = R.pipe(filterLetters, mapToLowercase);
+export const countLetters: Function = (str: string): LetterCount => {
+  const addOrInc = (freqs: LetterCount, c: string): LetterCount => {
+    const newFreqs = { ...freqs };
+    const currentCount = newFreqs[c] || 0;
+    newFreqs[c] = currentCount + 1;
+    return newFreqs;
+  };
+  const isLetter = (str: string): boolean =>
+    str.length === 1 && /[a-z]/i.test(str);
+  const filterLetters = (strs: string[]): string[] => R.filter(isLetter, strs);
+  const mapToLowercase = (strs: string[]): string[] =>
+    R.map((x: string): string => x.toLowerCase(), strs);
+  const mapAndFilter = R.pipe(filterLetters, mapToLowercase);
 
-    return R.reduce(addOrInc, {}, mapAndFilter(stringToArray(str)));
-}
+  return R.reduce(addOrInc, {}, mapAndFilter(stringToArray(str)));
+};
 
 /* Question 2 */
-export const isPaired: Function = (str : string) : boolean => {
-    const strArray = stringToArray(str);
-    
-    const checkLetter = (c : string, open : string, close: string) : number => c === open ? 1 : c === close ? -1 : 0;
-    const accByType = (open: string, close: string) => (acc : [boolean, number], curr : string) : [boolean, number] => {
-        const counter = acc[1] + checkLetter(curr, open, close);
-        return [counter < 0 ? false : true && acc[0] ,counter];
-    }
+export const isPaired: Function = (str: string): boolean => {
+  const strArray = stringToArray(str);
 
-    const checkBrackets = (strs : string[], open: string, close: string) : boolean =>  {
-        const outcome = R.reduce(accByType(open, close), [true, 0], strs);
-        return outcome[0] && outcome[1] === 0;
-    }
+  const checkLetter = (c: string, open: string, close: string): number =>
+    c === open ? 1 : c === close ? -1 : 0;
+  const accByType =
+    (open: string, close: string) =>
+    (acc: [boolean, number], curr: string): [boolean, number] => {
+      const counter = acc[1] + checkLetter(curr, open, close);
+      return [counter < 0 ? false : true && acc[0], counter];
+    };
 
-    return checkBrackets(strArray, '(', ')') && checkBrackets(strArray, '[', ']') && checkBrackets(strArray, '{', '}');
-}
+  const checkBrackets = (
+    strs: string[],
+    open: string,
+    close: string
+  ): boolean => {
+    const outcome = R.reduce(accByType(open, close), [true, 0], strs);
+    return outcome[0] && outcome[1] === 0;
+  };
+
+  return (
+    checkBrackets(strArray, "(", ")") &&
+    checkBrackets(strArray, "[", "]") &&
+    checkBrackets(strArray, "{", "}")
+  );
+};
 
 /* Question 3 */
 export type WordTree = {
-    root: string;
-    children: WordTree[];
-}
+  root: string;
+  children: WordTree[];
+};
 
-export const treeToSentence : undefined = undefined
+export const treeToSentence: Function = (t: WordTree): string => {
+  const treeToArray = (t: WordTree): Array<any> => {
+    return [t.root, R.map(treeToArray, t.children)];
+  };
 
+  const joinWords = (strs: string[]): string =>
+    strs.reduce(
+      (acc: string, curr: string, i: number): string =>
+        i === 0 ? curr : i === strs.length ? acc : acc + " " + curr,
+      ""
+    );
+
+  return joinWords(R.flatten(treeToArray(t)));
+};
