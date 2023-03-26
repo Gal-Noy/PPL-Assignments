cd77e58f6d74516f285ee19a7c7c1ade140a410b
Gal Noy <109943831+Gal-Noy@users.noreply.github.com>
2023-03-26 13:20:45 +0300

    Remove returns

--- Assignment1/src/part2/part2.ts
+++ Assignment1/src/part2/part2.ts
@@ -5,42 +5,28 @@ const stringToArray = R.split("");
 
 type LetterCount = Dictionary<number>;
 
+const filterLetters = (strs: string[]): string[] => R.filter((c) => /[a-z]/i.test(c), strs);
+const mapToLowercase = (strs: string[]): string[] => R.map((c) => R.toLower(c), strs);
+const mapAndFilter = R.pipe(filterLetters, mapToLowercase);
 /* Question 1 */
-export const countLetters: (str: string) => LetterCount = (
-  str: string
-): LetterCount => {
-  const filterLetters = (strs: string[]): string[] =>
-    R.filter((c) => /[a-z]/i.test(c), strs);
-  const mapToLowercase = (strs: string[]): string[] =>
-    R.map((c) => R.toLower(c), strs);
-  const mapAndFilter = R.pipe(filterLetters, mapToLowercase);
-  return R.countBy(R.toLower, mapAndFilter(stringToArray(str)));
-};
+export const countLetters: (str: string) => LetterCount = (str: string): LetterCount =>
+  R.countBy(R.toLower, mapAndFilter(stringToArray(str)));
 
+const handleStack = (stack: string[], open: string, close: string): string[] =>
+  R.last(stack) === open ? R.dropLast(1, stack) : R.concat(stack, [close]);
+const reducer = (acc: string[], curr: string): string[] =>
+  curr === ")"
+    ? handleStack(acc, "(", curr)
+    : curr === "]"
+    ? handleStack(acc, "[", curr)
+    : curr === "}"
+    ? handleStack(acc, "{", curr)
+    : curr === "(" || curr === "[" || curr === "{"
+    ? R.concat(acc, [curr])
+    : acc;
 /* Question 2 */
-export const isPaired: (str: string) => boolean = (str: string): boolean => {
-  const handleStack = (
-    stack: string[],
-    open: string,
-    close: string
-  ): string[] => {
-    return R.last(stack) === open
-      ? R.dropLast(1, stack)
-      : R.concat(stack, [close]);
-  };
-  const reducer = (acc: string[], curr: string): string[] => {
-    return curr === ")"
-      ? handleStack(acc, "(", curr)
-      : curr === "]"
-      ? handleStack(acc, "[", curr)
-      : curr === "}"
-      ? handleStack(acc, "{", curr)
-      : curr === "(" || curr === "[" || curr === "{"
-      ? R.concat(acc, [curr])
-      : acc;
-  };
-  return R.isEmpty(R.reduce(reducer, [], stringToArray(str)));
-};
+export const isPaired: (str: string) => boolean = (str: string): boolean =>
+  R.isEmpty(R.reduce(reducer, [], stringToArray(str)));
 
 /* Question 3 */
 export type WordTree = {
@@ -48,18 +34,9 @@ export type WordTree = {
   children: WordTree[];
 };
 
-export const treeToSentence: (t: WordTree) => string = (
-  t: WordTree
-): string => {
-  const treeToArray = (t: WordTree): Array<any> => {
-    return [t.root, R.map(treeToArray, t.children)];
-  };
+const treeToArray = (t: WordTree): Array<any> => [t.root, R.map(treeToArray, t.children)];
+const joinWords = (strs: string[]): string =>
+  R.reduce((acc: string, curr: string): string => R.concat(R.concat(acc, " "), curr), "", strs);
 
-  const joinWords = (strs: string[]): string =>
-    R.reduce(
-      (acc: string, curr: string): string => R.concat(R.concat(acc, " "), curr),
-      "",
-      strs
-    );
-  return R.trim(joinWords(R.flatten(treeToArray(t))));
-};
+export const treeToSentence: (t: WordTree) => string = (t: WordTree): string =>
+  R.trim(joinWords(R.flatten(treeToArray(t))));
--- Assignment1/src/part3/find.ts
+++ Assignment1/src/part3/find.ts
@@ -1,12 +1,4 @@
-import {
-  Result,
-  makeFailure,
-  makeOk,
-  bind,
-  either,
-  isOk,
-  isFailure,
-} from "../lib/result";
+import { Result, makeFailure, makeOk, bind, either, isOk, isFailure } from "../lib/result";
 import * as R from "ramda";
 
 /* Library code */
@@ -17,23 +9,13 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
   throw "No element found.";
 };
 
-export const findResult: <T>(pred: (x: T) => boolean, a: T[]) => Result<T> = <
-  T
->(
+export const findResult: <T>(pred: (x: T) => boolean, a: T[]) => Result<T> = <T>(
   pred: (x: T) => boolean,
   a: T[]
 ): Result<T> => {
   const reducer = (acc: [Result<T>, boolean], curr: T): [Result<T>, boolean] =>
-    acc[1]
-      ? acc
-      : pred(curr)
-      ? [makeOk(curr), true]
-      : [makeFailure("No element found."), false];
-  const finalTuple: [Result<T>, boolean] = R.reduce(
-    reducer,
-    [makeFailure("No element found."), false],
-    a
-  );
+    acc[1] ? acc : pred(curr) ? [makeOk(curr), true] : [makeFailure("No element found."), false];
+  const finalTuple: [Result<T>, boolean] = R.reduce(reducer, [makeFailure("No element found."), false], a);
   return finalTuple[0];
 };
 
@@ -47,19 +29,12 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
   }
 };
 
-export const returnSquaredIfFoundEven_v2: (a: number[]) => Result<number> = (
-  a: number[]
-): Result<number> => {
+export const returnSquaredIfFoundEven_v2: (a: number[]) => Result<number> = (a: number[]): Result<number> => {
   const res: Result<number> = findResult((x: number) => x % 2 === 0, a);
-  return bind(
-    res,
-    (value: number): Result<number> => (isOk(res) ? makeOk(value * value) : res)
-  );
+  return bind(res, (value: number): Result<number> => (isOk(res) ? makeOk(value * value) : res));
 };
 
-export const returnSquaredIfFoundEven_v3: (a: number[]) => number = (
-  a: number[]
-): number => {
+export const returnSquaredIfFoundEven_v3: (a: number[]) => number = (a: number[]): number => {
   const res: Result<number> = findResult((x: number) => x % 2 === 0, a);
   return either(
     res,
