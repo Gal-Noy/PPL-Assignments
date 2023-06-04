85ed21a61e783b9fd304e57f149eac61f3962e6b
Gal Noy <109943831+Gal-Noy@users.noreply.github.com>
2023-06-04 18:12:21 +0300

    finished Q2 ass4

--- Assignment4/part2/src/part2.ts
+++ Assignment4/part2/src/part2.ts
@@ -1,51 +1,65 @@
 // PPL 2023 HW4 Part2
 
-// Q 2.1 
+// Q 2.1
 
 // Specify the return type.
-export const delayedSum = async (a: number, b: number, delay: number) => 
-   "TODO";
+export const delayedSum = async (a: number, b: number, delay: number) =>
+  new Promise((resolve) => setTimeout(() => resolve(a + b), delay));
 
 export const testDelayedSum = () => {
-    console.log("TODO testDelayedSum");
- }
- 
+  const start = Date.now();
+  delayedSum(1, 2, 1000).then(() => {
+    const end = Date.now();
+    return end - start >= 1000;
+  });
+};
 
 // Q 2.2
 
 // Values returned by API calls.
 export type Post = {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-}
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
 
 // When invoking fetchData(postsUrl) you obtain an Array Post[]
 // To obtain an array of posts
-export const postsUrl = 'https://jsonplaceholder.typicode.com/posts'; 
+export const postsUrl = "https://jsonplaceholder.typicode.com/posts";
 
 // Append the desired post id.
-export const postUrl = 'https://jsonplaceholder.typicode.com/posts/'; 
+export const postUrl = "https://jsonplaceholder.typicode.com/posts/";
 
 // When invoking fetchData(invalidUrl) you obtain an error
-export const invalidUrl = 'https://jsonplaceholder.typicode.com/invalid';
+export const invalidUrl = "https://jsonplaceholder.typicode.com/invalid";
 
 // Depending on the url - fetchData can return either an array of Post[] or a single Post.
 // Specify the return type without using any.
-export const fetchData = async (url: String) => 
-    "TODO";
+export const fetchData = async (url: string) =>
+  fetch(url).then((res) => {
+    if (res.ok) return res.json();
+    else throw new Error("Error");
+  });
 
 export const testFetchData = () => {
-    console.log("TODO testFetchData");
-}
+  fetchData(postsUrl)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  fetchData(postUrl + "1")
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  fetchData(invalidUrl)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
 // Q 2.3
 
 // Specify the return type.
-export const fetchMultipleUrls = async (urls: string[]) => 
-    "TODO";
+export const fetchMultipleUrls = async (urls: string[]) => Promise.all(urls.map((url) => fetchData(url)));
 
-export const testFetchMultipleUrls = () => {
-    console.log("TODO testFetchData");
-}
+export const testFetchMultipleUrls = () =>
+  fetchMultipleUrls(Array.from({ length: 20 }, (_, i) => i + 1).map((i) => postUrl + i))
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
--- Assignment4/part2/test/test.test.ts
+++ Assignment4/part2/test/test.test.ts
@@ -1,40 +1,61 @@
-import { describe, expect, test } from '@jest/globals'
-import {
-    delayedSum, Post, postsUrl, postUrl, invalidUrl, fetchData, fetchMultipleUrls
-} from '../src/part2';
+import { describe, expect, test } from "@jest/globals";
+import { delayedSum, Post, postsUrl, postUrl, invalidUrl, fetchData, fetchMultipleUrls } from "../src/part2";
 
-describe('Assignment 4 Part 2', () => {
-    describe('Q2.1 delayedSum (6 points)', () => {
-        test('delayedSum returns the sum', () => {
+describe("Assignment 4 Part 2", () => {
+  describe("Q2.1 delayedSum (6 points)", () => {
+    test("delayedSum returns the sum", () => {
+      delayedSum(1, 2, 1000).then((result) => {
+        expect(result).toBe(3);
+      });
+    });
+    test("delayedSum waits at least the specified delay", () => {
+      const start = Date.now();
+      delayedSum(1, 2, 1000).then(() => {
+        const end = Date.now();
+        expect(end - start).toBeGreaterThanOrEqual(1000);
+      });
+    });
+  });
 
-        })
-        test('delayedSum waits at least the specified delay', () => {
+  describe("Q2.2 fetchData (12 points)", () => {
+    test("successful call to fetchData with array result", async () =>
+      fetchData(postsUrl).then((res) => expect(res.length).toBe(100)));
 
-        })
-    })
+    test("successful call to fetchData with Post result", async () => {
+      const expected: Post = {
+        userId: 1,
+        id: 1,
+        title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        body:
+          "quia et suscipit\n" +
+          "suscipit recusandae consequuntur expedita et cum\n" +
+          "reprehenderit molestiae ut ut quas totam\n" +
+          "nostrum rerum est autem sunt rem eveniet architecto",
+      };
 
-    describe('Q2.2 fetchData (12 points)', () => {
-        test('successful call to fetchData with array result', async () => {
-        })
+      fetchData(postUrl + "1").then((res) => expect(res).toEqual(expected));
+    });
 
-        test('successful call to fetchData with Post result', async () => {
-        })
+    test("failed call to fechData", async () =>
+      fetchData(invalidUrl).catch((err) => expect(err.message).toBe("Error")));
+  });
 
-        test('failed call to fechData', async () => {
-        })
+  describe("Q2.3 fetchMultipleUrls (12 points)", () => {
+    test("successful call to fetchMultipleUrls", async () =>
+      fetchMultipleUrls(Array.from({ length: 20 }, (_, i) => i + 1).map((i) => postUrl + i)).then((res) =>
+        expect(res.length).toBe(20)
+      ));
 
-    })
+    test("successful call to fetchMultipleUrls: verify results are in the expected order ", async () => {
+      fetchMultipleUrls(Array.from({ length: 20 }, (_, i) => i + 1).map((i) => postUrl + i)).then((res) =>
+        expect(res.map((post: Post) => post.id)).toEqual(Array.from({ length: 20 }, (_, i) => i + 1))
+      );
+    });
 
-    describe('Q2.3 fetchMultipleUrls (12 points)', () => {
-        test('successful call to fetchMultipleUrls', async () => {
-        })
-
-        test('successful call to fetchMultipleUrls: verify results are in the expected order ', async () => {
-        })
-
-        test('failed call to fetchMultipleUrls', async () => {
-        })
-
-    })
+    test("failed call to fetchMultipleUrls", async () => {
+      fetchMultipleUrls(Array.from({ length: 101 }, (_, i) => i + 1).map((i) => postUrl + i))
+        .then((res) => expect(true).toEqual(false))
+        .catch((err) => expect(err.message).toBe("Error"));
+    });
+  });
 });
-
